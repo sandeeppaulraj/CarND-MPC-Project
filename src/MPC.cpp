fee990341d8957d1377665c796c5296a78b58556
sandeeppaulraj <sandeeppaulraj@gmail.com>
2018-03-30 14:00:25 -0700

    complete first revision of solve

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -279,5 +279,14 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   //
   // {...} is shorthand for creating a vector, so auto x1 = {1.0,2.0}
   // creates a 2 element double vector.
-  return {};
+    vector<double> result;
+
+  result.push_back(solution.x[delta_start]);
+  result.push_back(solution.x[a_start]);
+
+  for ( int i = 0; i < N - 2; i++ ) {
+    result.push_back(solution.x[x_start + i + 1]);
+    result.push_back(solution.x[y_start + i + 1]);
+  }
+  return result;
 }
